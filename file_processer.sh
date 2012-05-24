311c54d0833f1cd1f9d4db247d33d18afca6d6a0
kitamura <kitamura@example.com>
2012-05-24 11:03:31 +0900

    clean up some comment

--- file_processer.sh
+++ file_processer.sh
@@ -90,9 +90,6 @@ do
 
 				COMMAND="$FF264 $WORKING_DIR/$F $F $WORKING_DIR/${F/.*/_1280x720_tc.mov}"
 
-				#VFOPT1="drawtext=fontfile=/usr/share/fonts/liberation/LiberationMono-Regular.ttf: timecode='00\:00\:00\:00': r=23.98: x=(w-2*tw)/4: y=h-(3*lh): fontcolor=white: box=0: fontsize=40: box=1: boxcolor=black@0.6"
-				#VFOPT2="drawtext=fontfile=/usr/share/fonts/liberation/LiberationMono-Regular.ttf: text='${F/.*/}': x=(w-2*tw)/4: y=h-(1.5*lh): fontcolor=white: box=0: fontsize=40: box=1: boxcolor=black@0.6"
-				#COMMAND="ffmpeg -i $WORKING_DIR/$F -vcodec libx264 -vprofile baseline -b:v 2400k -s 1280x720 -acodec aac -strict experimental -ab 128k -vf \"$VFOPT1, $VFOPT2\" $WORKING_DIR/${F/.*/_1280x720_tc.mov}"
 				echo "$COMMAND"
 				#$COMMAND
 				$FF264 "$WORKING_DIR/$F" "$F" "$WORKING_DIR/${F/.*/_1280x720_tc.mov}" "$PARAMFILE"
