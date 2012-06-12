1bd3ce87566491c1bab39ca3c5ecb0a22e9ec842
kitamura <kitamura@example.com>
2012-06-13 04:56:05 +0900

    modified some points

--- file_processer_simple.sh
+++ file_processer_simple.sh
@@ -27,66 +27,67 @@ fi
 
 
 
+
 #2. get list of files to be transcoded
-for F in $DROPDIR/*.mov $DROPDIR/*.MOV
+for F in "$DROPDIR"/*.mov "$DROPDIR"/*.MOV
 do
-	if [ -e "$F" ]; then
 
-		#3. skip files including "_1280x720_tc_fs" in its file name
-		if [[ "$F" =~ "$POSTFIX" ]]; then
-			continue;
-		fi
+	echo "processing $F ... 1"
 
-		#4. skip files if file of name with "_1280x720_tc_fs" exisits
-		FILE_NAME="${F##*/}"
-		FILE_PREFIX="${FILE_NAME%.*}"
-		FILE_EXT="${FILE_NAME##*.}"
-		if [ -e "$DROPDIR/${FILE_PREFIX}${POSTFIX}.mov" ]; then
-			continue;
-		fi
+	#3. skip files including "_1280x720_tc_fs" in its file name
+	if [[ "$F" =~ "$POSTFIX" ]]; then
+		continue;
+	fi
+
+	#4. skip files if file of name with "_1280x720_tc_fs" exisits
+	FILE_NAME="${F##*/}"
+	FILE_PREFIX="${FILE_NAME%.*}"
+	FILE_EXT="${FILE_NAME##*.}"
+	if [ -e "$DROPDIR/${FILE_PREFIX}${POSTFIX}.mov" ]; then
+		continue;
+	fi
 
 
 
-		#5. check if the transfer was done
-		SIZE1=`wc -c "$F" | awk '{print $1}'`
-		echo "SIZE1 = $SIZE1"
-		sleep 1
-		SIZE2=`wc -c "$F" | awk '{print $1}'`
-		echo "SIZE2 = $SIZE2"
+	#5. check if the transfer was done
+	SIZE1=`wc -c "$F" | awk '{print $1}'`
+	echo "SIZE1 = $SIZE1"
+	sleep 1
+	SIZE2=`wc -c "$F" | awk '{print $1}'`
+	echo "SIZE2 = $SIZE2"
 
-		if [ $SIZE1 = $SIZE2 ]; then
+	if [ $SIZE1 = $SIZE2 ]; then
 
 		#DEBUG
 		echo "processing $F ... "
 
-			#6. do ffmpeg transcoding
-			COMMAND="$FF264 $F $FILE_PREFIX ${F/.*/_1280x720_tc.mov}"
-			echo "$COMMAND"
-			#$COMMAND
-			$FF264 "$F" "$FILE_PREFIX" "${F/.*/_1280x720_tc.mov}" "$PARAMFILE"
+		#6. do ffmpeg transcoding
+		COMMAND="$FF264 $F $FILE_PREFIX ${F/.*/_1280x720_tc.mov}"
+		echo "$COMMAND"
+		#$COMMAND
+		$FF264 "$F" "$FILE_PREFIX" "${F/.*/_1280x720_tc.mov}" "$PARAMFILE"
 
-			#7. do qt-fast-ing
-			COMMAND="$QTFAST ${F/.*/_1280x720_tc.mov} ${F/.*/$POSTFIX.mov}"
-			echo "$COMMAND"
-			$QTFAST "${F/.*/_1280x720_tc.mov}" "${F/.*/$POSTFIX.mov}"
+		#7. do qt-fast-ing
+		COMMAND="$QTFAST ${F/.*/_1280x720_tc.mov} ${F/.*/$POSTFIX.mov}"
+		echo "$COMMAND"
+		$QTFAST "${F/.*/_1280x720_tc.mov}" "${F/.*/$POSTFIX.mov}"
 
-			#8. remove temporary file
-			COMMAND="rm ${F/.*/_1280x720_tc.mov}"
-			echo "$COMMAND"
-			#$COMMAND
-			rm "${F/.*/_1280x720_tc.mov}"
+		#8. remove temporary file
+		COMMAND="rm ${F/.*/_1280x720_tc.mov}"
+		echo "$COMMAND"
+		#$COMMAND
+		rm "${F/.*/_1280x720_tc.mov}"
 
-			#9. chmod for one to rwrite and read
-			echo "chmod 666"
-			chmod 666 "${F/.*/$POSTFIX.mov}"
+		#9. chmod for one to rwrite and read
+		echo "chmod 666"
+		chmod 666 "${F/.*/$POSTFIX.mov}"
 
-		else
+	else
 
-			echo "file $F is now trasferring..."
-
-		fi
+		echo "file $F is now trasferring..."
 
 	fi
+
 done
 
 #rm process lock file
--- file_sharing.sh
+++ file_sharing.sh
@@ -3,6 +3,10 @@
 LOCAL_DIR="/data/KMD UCSD collaboration documentary/_FILE_SHARED"
 REMOTE_DIR="_FILE_SHARED"
 
+#LOCAL_DIR2="/data/KMD UCSD collaboration documentary/Digital Distribution"
+#REMOTE_DIR2="Digital Distribution"
+
+
 FTP_SERVER="67.58.35.229"
 
 LOCK="$LOCAL_DIR/.file_sharing"
@@ -15,13 +19,23 @@ fi
 
 touch "$LOCK"
 
-echo "pull from remote server ..."
+echo "pull from remote server ... [$LOCAL_DIR <==> $REMOTE_DIR]"
 
 #LFTP_COMMAND=""
 lftp -u collaboration,documentary "$FTP_SERVER" <<END
 mirror -np  "$REMOTE_DIR"  "$LOCAL_DIR"
 mirror -nRp "$LOCAL_DIR"  "$REMOTE_DIR"
 END
+echo "done ... [$LOCAL_DIR <==> $REMOTE_DIR]"
+
+#echo "pull from remote server ... [$LOCAL_DIR2 <==> $REMOTE_DIR2]"
+#LFTP_COMMAND=""
+#lftp -u collaboration,documentary "$FTP_SERVER" <<END
+#mirror -np  "$REMOTE_DIR2"  "$LOCAL_DIR2"
+#mirror -nRp "$LOCAL_DIR2"  "$REMOTE_DIR2"
+#END
+#echo "done ... [$LOCAL_DIR2 <==> $REMOTE_DIR2]"
+
 
 #echo "push to remote server"
 
